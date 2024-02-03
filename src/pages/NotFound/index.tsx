1e678939b0785c326a4c0bbf3965828d7604d8f1
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-03 22:49:04 +0100

    Add routes

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+const NotFound = memo((): JSX.Element => {
+  return <h2>Page not found</h2>;
+});
+
+export { NotFound };
---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tasks = (): JSX.Element => {
+  return <h2>Tasks</h2>;
+};
+
+export { Tasks };
