5de9d4131f7b1e2cc74309c1c52ecd7ecb2aead9
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-05 10:24:09 +0100

    Create simple login page

---
+++ src/pages/LogIn/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  padding-top: 8rem;
+
+  .MuiFormControl-root {
+    margin: 1rem 0 1rem 0;
+  }
+`;
+
+export { Wrapper };
