c0c4a79be8082ffcfb032f6f1278815431e7751e
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-05 10:27:04 +0100

    Create simple login page (#2)

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,7 +1,34 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { TextField, Button } from "@mui/material";
+
+import { Wrapper } from "./styled";
 
 const LogIn = memo((): JSX.Element => {
-  return <h2>Log in</h2>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Wrapper>
+      <h2>Log in</h2>
+      <TextField
+        id="outlined-basic"
+        variant="outlined"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+        placeholder="Email"
+      />
+      <TextField
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+        placeholder="Password"
+      />
+      <Button variant="outlined">Log in</Button>
+    </Wrapper>
+  );
 });
 
 export { LogIn };
